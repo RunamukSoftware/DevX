4a20acbf4717c86f38eb105c62e5eb94ff1734b8
Tony Green <runamuk0@msn.com>
2018-11-15 07:28:12 -0800

    Initial revision

---
+++ AdoNetTest/AdoNetTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection();
+
+            //conn.ConnectionString = @"Provider=SQLNCLI11;Server=PER730XD.etlabnet.local\SS17;Database=portal;Integrated Security=SSPI;DataTypeCompatibility=80;MARS Connection=True;";
+            //conn.ConnectionString = @"Server=PER730XD.etlabnet.local\SS17;Database=portal;Integrated Security=SSPI;";
+            //conn.ConnectionString = @"Server=PER730XD.etlabnet.local\SS17;Database=portal;User Id=portal;Password=Portal_1;";
+            conn.ConnectionString = @"Provider=SQLNCLI12;Server=PER730XD.etlabnet.local\SS17;Database=portal;User Id=portal;Password=Portal_1;";
+            conn.Open();
+            Console.WriteLine(conn);
+
+            conn.Close();
+        }
+    }
+}

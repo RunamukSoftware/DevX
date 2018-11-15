86a2bb476a8d177034cdff228c86ca998e1792cc
Tony Green <runamuk0@msn.com>
2018-11-15 07:30:52 -0800

    Initial revision

---
+++ gRPCConsoleApp1/gRPCConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace gRPCConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerServiceDefinition.Builder builder = new ServerServiceDefinition.Builder();
+            Grpc.Core.Server server = new Server();
+            server.Services.Add(ServerServiceDefinition.Builder
+
+        }
+    }
+}
